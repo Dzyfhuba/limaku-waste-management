ac693d9734c148b86e2625232308afcaa2026953
Dzyfhuba <hafidz21ub@gmail.com>
2022-06-30 23:45:51 +0700

    initial

---
+++ resources/js/Pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Guest from '@/Layouts/Guest';
+import { Head } from '@inertiajs/inertia-react';
+
+export default function LandingPage(props) {
+    return (
+        <Guest>
+            asd
+        </Guest>
+    );
+}
